9cde8cdc1712112a367dc06d99cfd3a6e267e09f
matheusrfferreira <mrff@tuta.io>
2023-09-07 03:54:01 -0300

    fix: fs functions

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,13 +1,6 @@
-import path from 'path';
-import { promises as fs } from 'fs';
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const jsonDirectory = path.join(process.cwd(), 'src')
-  const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8')
-  // return NextResponse.json({
-  //   fileContents
-  // })
   return NextResponse.json({
     data: 'One Piece API comming soon!'
   })
